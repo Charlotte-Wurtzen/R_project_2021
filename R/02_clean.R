bdeb219e8f16095cff3245c352fdd29fe177b7dd
Charlotte-Wurtzen <s174564@student.dtu.dk>
2021-04-25 15:18:08 +0000

    data cleaning

--- R/02_clean.R
+++ R/02_clean.R
@@ -6,14 +6,14 @@ rm(list = ls())
 library("tidyverse")
 
 # Load data ---------------------------------------------------------------
-x <- read_tsv(file = "2021_group_7/data/golub_x.tsv.gz")
-y <- read_tsv(file = "2021_group_7/data/golub_y.tsv.gz")
+x <- read_tsv(file = "data/golub_x.tsv.gz")
+y <- read_tsv(file = "data/golub_y.tsv.gz")
 
 
 # Wrangle data ------------------------------------------------------------
-gravier_clean <- bind_cols(x,y) 
+golub_clean <- bind_cols(x,y) 
 
 
 # Write data --------------------------------------------------------------
-write_tsv(x = gravier_clean,
-          path = "2021_group_7/data/02_golub_clean.tsv.gz")
+write_tsv(x = golub_clean,
+          path = "data/02_golub_clean.tsv.gz")
