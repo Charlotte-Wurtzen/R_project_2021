e130a4797e8ffa2be4f30200e691493a49a9f14f
simone-majkengit config --global user.email s174596@student.dtu.dkgit config --global user.name simone-majken <s174596@student.dtu.dk>
2021-04-26 10:36:11 +0000

    plots

--- R/07_analysis_plots.R
+++ R/07_analysis_plots.R
@@ -15,19 +15,60 @@ source(file = "R/99_project_functions.R")
 # pivot longer 
 golub_data_long <- longer(golub_clean_aug)
 
-# Histogram ----------------------------------------------------------------
+# Histograms ----------------------------------------------------------------
 golub_data_long %>% 
   count(type) %>% 
   ggplot(aes(x= type, y=n, fill= type))+
   geom_col()
 
+ALL <- 
+  filter(ggolub_data_long, type=0 ) %>% 
+  count(Allele_F_1_2) %>% 
+  mutate(perc = n / 72 * 100)
+
+g1 <- ALL %>% 
+  ggplot(mapping = aes(x = perc, y = reorder(Allele_F_1_2, perc))) + 
+  geom_bar(stat = "identity") + 
+  theme_bw(base_size = 8) +
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  labs(title = "HLA-A") + 
+  xlab(NULL) + 
+  ylab(label="Allele") +
+  xlim(0, 33)
+
+
+AML <- 
+  filter(allele_data_long, type=1) %>% 
+  count(type) %>% 
+  mutate(perc = n / 72 * 100)
+
+g2 <- AML %>% 
+  ggplot(mapping = aes(x = perc, y = reorder(Allele_F_1_2, perc))) + 
+  geom_bar(stat = "identity") + 
+  theme_bw(base_size = 8) +
+  theme(plot.title = element_text(hjust = 0.5)) + 
+  labs(title = "HLA-B") + 
+  xlab("Observed Allele Frequency (within each gene) [%]") + 
+  ylab(NULL) + 
+  xlim(0, 33)
+
+
+g1 + g2 
+
 # Scatter plot ------------------------------------------------------------
-ggplot(data = golub_data_long, mapping = aes(x =gene, y = norm_expr_level, colour = norm_expr_level)) +
+  # sample 100 random genes
+set.seed(12345)
+golub_data_long_sample <- 
+  golub_data_long %>% 
+  sample_n(100)
+
+ggplot(data = golub_data_long_sample, mapping = aes(x =gene, y = norm_expr_level, colour = norm_expr_level)) +
   geom_point() + 
   scale_color_gradient(low="blue", high="red")+
   theme_minimal()+
-  ylab(label="Gene") +
-  xlab(label="Normalized Expression Level") +
-  labs(title = "?", caption = "Group 7")
+  ylab(label="Normalized Expression Level") +
+  xlab(label="Gene") +
+  labs(title = "?", caption = "Group 7")+
+theme(legend.position = 'bottom', axis.text.x = element_text(angle = 45, hjust=1))
 
 
