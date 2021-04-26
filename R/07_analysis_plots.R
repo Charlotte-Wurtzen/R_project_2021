fcc237ee29aba660f300f8acd822f65ef4ed5c46
simone-majkengit config --global user.email s174596@student.dtu.dkgit config --global user.name simone-majken <s174596@student.dtu.dk>
2021-04-26 08:18:00 +0000

    plots

--- R/07_analysis_plots.R
+++ R/07_analysis_plots.R
@@ -12,7 +12,8 @@ golub_clean_aug <- read_tsv(file = "data/03_golub_clean_aug.tsv.gz")
 golub_data_long <- golub_clean_aug %>% 
   pivot_longer(cols = c(-type,-value), 
                names_to = "gene", 
-               values_to = "expr_level") 
+               values_to = "expr_level") %>% 
+              mutate(norm_expr_level = (expr_level - mean(expr_level))/sd(expr_level) )
 # Histogram ----------------------------------------------------------------
 golub_data_long %>% 
   count(value) %>% 
@@ -20,7 +21,7 @@ golub_data_long %>%
   geom_col()
 
 # Scatter plot ------------------------------------------------------------
-ggplot(data = golub_data_long, mapping = aes(x = str_extract(gene,"5_at"), y = value, colour = expr_level)) +
+ggplot(data = golub_data_long, mapping = aes(x = str_extract(gene,"5_at"), y = value, colour = norm_expr_level)) +
   geom_point() + 
   scale_color_gradient(low="blue", high="red")+
   theme_minimal()+
