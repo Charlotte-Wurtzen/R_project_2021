6df41d6272d5fc196c68b743d5724611f174a6b2
simone-majkengit config --global user.email s174596@student.dtu.dkgit config --global user.name simone-majken <s174596@student.dtu.dk>
2021-04-26 10:53:49 +0000

    slet ikke seje plots

--- R/07_analysis_plots.R
+++ R/07_analysis_plots.R
@@ -4,6 +4,7 @@ rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
+library("patchwork")
 
 # Load data ---------------------------------------------------------------
 golub_clean_aug <- read_tsv(file = "data/03_golub_clean_aug.tsv.gz")
@@ -21,34 +22,32 @@ golub_data_long %>%
   ggplot(aes(x= type, y=n, fill= type))+
   geom_col()
 
-ALL <- 
-  filter(ggolub_data_long, type=0 ) %>% 
-  count(Allele_F_1_2) %>% 
-  mutate(perc = n / 72 * 100)
+# sample 100 random genes
+set.seed(12345)
+golub_data_long_sample <- 
+  golub_data_long %>% 
+  sample_n(50)
 
-g1 <- ALL %>% 
-  ggplot(mapping = aes(x = perc, y = reorder(Allele_F_1_2, perc))) + 
+g1 <- golub_data_long_sample %>%
+  ggplot(mapping = aes(x = norm_expr_level, y = gene)) + 
   geom_bar(stat = "identity") + 
   theme_bw(base_size = 8) +
   theme(plot.title = element_text(hjust = 0.5)) + 
-  labs(title = "HLA-A") + 
+  labs(title = "ALL") + 
   xlab(NULL) + 
-  ylab(label="Allele") +
+  ylab(label="Gene expression") +
   xlim(0, 33)
 
 
-AML <- 
-  filter(allele_data_long, type=1) %>% 
-  count(type) %>% 
-  mutate(perc = n / 72 * 100)
 
-g2 <- AML %>% 
-  ggplot(mapping = aes(x = perc, y = reorder(Allele_F_1_2, perc))) + 
+
+g2 <- golub_data_long_sample %>%
+  ggplot(mapping = aes(x = norm_expr_level, y = gene)) + 
   geom_bar(stat = "identity") + 
   theme_bw(base_size = 8) +
   theme(plot.title = element_text(hjust = 0.5)) + 
-  labs(title = "HLA-B") + 
-  xlab("Observed Allele Frequency (within each gene) [%]") + 
+  labs(title = "AML") + 
+  xlab("Gene expression level") + 
   ylab(NULL) + 
   xlim(0, 33)
 
@@ -56,11 +55,6 @@ g2 <- AML %>%
 g1 + g2 
 
 # Scatter plot ------------------------------------------------------------
-  # sample 100 random genes
-set.seed(12345)
-golub_data_long_sample <- 
-  golub_data_long %>% 
-  sample_n(100)
 
 ggplot(data = golub_data_long_sample, mapping = aes(x =gene, y = norm_expr_level, colour = norm_expr_level)) +
   geom_point() + 
