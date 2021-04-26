706215605acf9ac6f5236ff5317cd420bc75531f
simone-majkengit config --global user.email s174596@student.dtu.dkgit config --global user.name simone-majken <s174596@student.dtu.dk>
2021-04-26 09:55:58 +0000

    plots

--- R/07_analysis_plots.R
+++ R/07_analysis_plots.R
@@ -8,26 +8,26 @@ library("tidyverse")
 # Load data ---------------------------------------------------------------
 golub_clean_aug <- read_tsv(file = "data/03_golub_clean_aug.tsv.gz")
 
+# Define functions --------------------------------------------------------
+source(file = "R/99_project_functions.R")
+
 # Wrangle data ------------------------------------------------------------
-golub_data_long <- golub_clean_aug %>% 
-  pivot_longer(cols = c(-type,-value), 
-               names_to = "gene", 
-               values_to = "expr_level") %>% 
-              mutate(norm_expr_level = (expr_level - mean(expr_level))/sd(expr_level) )
+# pivot longer 
+golub_data_long <- longer(golub_clean_aug)
 
 # Histogram ----------------------------------------------------------------
 golub_data_long %>% 
-  count(value) %>% 
-  ggplot(aes(x= value, y=n, fill= value))+
+  count(type) %>% 
+  ggplot(aes(x= type, y=n, fill= type))+
   geom_col()
 
 # Scatter plot ------------------------------------------------------------
-ggplot(data = golub_data_long, mapping = aes(x = gene, y = value, colour = expr_level)) +
+ggplot(data = golub_data_long, mapping = aes(x =gene, y = norm_expr_level, colour = norm_expr_level)) +
   geom_point() + 
   scale_color_gradient(low="blue", high="red")+
   theme_minimal()+
-  ylab(label="?") +
-  xlab(label="?") +
+  ylab(label="Gene") +
+  xlab(label="Normalized Expression Level") +
   labs(title = "?", caption = "Group 7")
 
 
