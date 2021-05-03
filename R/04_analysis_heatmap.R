315ead8e876585283df0fced9cf3c5bf37ab624e
Charlotte-Wurtzen <s174564@student.dtu.dk>
2021-05-03 08:50:44 +0000

    heatmap

--- R/04_analysis_heatmap.R
+++ R/04_analysis_heatmap.R
@@ -8,37 +8,49 @@ library("tidyverse")
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
 
+
 # Load data ---------------------------------------------------------------
 #golub_clean_aug <- read_tsv(file = "data/03_golub_clean_aug.tsv.gz")
 golub_top_genes <- read_tsv(file = "data/06_top_genes.tsv.gz")
 
-# Wrangle data ------------------------------------------------------------
-golub_top_genes_wide <- golub_top_genes %>% 
-  pivot_wider(id_cols = NULL,names_from = gene,values_from = norm_expr_level)
 
-#range(1,71)
-#rank(x, ties.method = "first")
+# Wrangle data ------------------------------------------------------------
+
+# pivot wider and add patient id
+golub_top_genes_wide <- golub_top_genes %>% 
+  pivot_wider(names_from = gene,values_from = norm_expr_level) %>% 
+  unnest() %>% 
+  mutate(id = row_number()) %>% 
+  relocate(id)
+
+# pivot longer
+golub_top_genes_long <- golub_top_genes_wide %>% 
+  pivot_longer(cols = -c(id,type),
+               names_to = "gene", 
+               values_to = "norm_expr_level") 
+  
 
 
 # Visualise data ----------------------------------------------------------
-golub_top_genes %>% 
+golub_top_genes_long %>% 
   
-  ggplot(mapping = aes(x = rownames(golub_top_genes), y = gene, fill = norm_expr_level)) +
+  ggplot(mapping = aes(x = id, y = gene, fill = norm_expr_level)) +
   geom_tile() +
   
   theme_classic(base_size = 12) +
   ggtitle("Normalized gene expression levels distinguishing ALL and AML") + 
   scale_fill_gradient2(low = "blue", high = "red", mid = "white",midpoint = 2) +
-  xlab(label = "Type") + 
+  xlab(label = "Patient id") + 
   ylab(label = "Genes") + 
   labs(fill = "Normalized expression level") +
   theme(legend.position="bottom") +
   
   # facet_grid makes two panels, one for ALL, one for AML:
-  facet_grid(~ type, switch = "x", scales = "free_x", space = "free_x") + 
-  theme(axis.text.x = element_blank()) 
+  facet_grid(~ type, switch = "x", scales = "free_x", space = "free_x")  
+  #theme(axis.text.x = element_blank()) 
 
 
+# save plot ----------------------------------------------------------
 ggsave(filename = "results/04_heatmap.png", width = 16, height = 9, dpi = 72)
 
 
