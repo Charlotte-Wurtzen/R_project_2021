3b37be0f009bb86cb558af81ddaebd77597b1e9b
JulieMariaJ <s174595@student.dtu.dk>
2021-05-05 11:07:54 +0000

    k means into pca

--- R/05_analysis_PCA.R
+++ R/05_analysis_PCA.R
@@ -5,6 +5,7 @@ rm(list = ls())
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
 library("cowplot")
+library("patchwork")
 
 
 # Define functions --------------------------------------------------------
@@ -73,6 +74,48 @@ plot3 <- pca_fit %>%
     expand = expansion(mult = c(0, 0.01))) +
   theme_minimal_hgrid(12)
 
+# Kmeans analysis ------------------------------------------------------------
+
+pca_aug <- pca_fit %>% 
+  augment(golub_clean_aug)
+
+k_type <- pca_aug %>%
+  select(contains("_at"),contains("_st")) %>%
+  kmeans(centers = 2)
+k_type
+
+pca_aug_k_type <- k_type %>%
+  augment(pca_aug) %>% 
+  rename(cluster_type = .cluster)
+pca_aug_k_type
+
+k_pca <- pca_aug_k_type %>%
+  select(.fittedPC1, .fittedPC2) %>%
+  kmeans(centers = 2)
+k_pca
+
+pca_aug_k_type_pca <- k_pca %>%
+  augment(pca_aug_k_type) %>% 
+  rename(cluster_pca = .cluster)
+pca_aug_k_type_pca
+
+# plot type + clustering based on type and pca 
+p1 <- pca_aug %>% 
+  ggplot(aes(x = .fittedPC1, y = .fittedPC2, colour = factor(type))) +
+  geom_point() +
+  theme(legend.position = "bottom")
+
+p2 <- pca_aug_k_type_pca %>%
+  ggplot(aes(x = .fittedPC1, y = .fittedPC2, colour = cluster_type)) +
+  geom_point() +
+  theme(legend.position = "bottom")
+
+p3 <- pca_aug_k_type_pca %>%
+  ggplot(aes(x = .fittedPC1, y = .fittedPC2, colour = cluster_pca)) +
+  geom_point() +
+  theme(legend.position = "bottom")
+
+kmeans_plot <- p1 + p2 + p3
 
 # Write data --------------------------------------------------------------
 ggsave("results/05_PCA_plot1.png",
@@ -80,6 +123,9 @@ ggsave("results/05_PCA_plot1.png",
 
 ggsave("results/05_PCA_plot2.png",
        plot = plot2)
+
+ggsave("results/05_kmeans_plot1.png",
+       plot = kmeans_plot)
 
 ggsave("results/05_PCA_plot3.png",
        plot = plot3)
